7eaf76e2bbf2290703dae8fab0c1d389c105aaae
Nicole Orchard <norchard24@gmail.com>
2017-06-06 23:22:23 -0400

    Added error handling to http request in get_data.rb. Added acl 'public-read' grant.

--- get-data/get_data.rb
+++ get-data/get_data.rb
@@ -7,14 +7,14 @@ require 'dotenv'
 Dotenv.load
 
 uri = URI('https://www.nycgovparks.org/bigapps/DPR_PublicArt_001.json')
-json = Net::HTTP.get(uri)
+response = Net::HTTP.get_response(uri)
 
-File.open("temp.json", "w") { |temp| temp.write(json) }
+if response.code != "200"
+  $stderr.puts "Failed to read response: #{response}"
+  exit 1
+end
 
 s3 = Aws::S3::Resource.new(region:'us-east-1')
 bucket = s3.bucket(ENV['S3_BUCKET'])
-obj = bucket.object('test/test/DPR_PublicArt.json')
-obj.delete
-obj.upload_file("temp.json")
-
-File.delete('temp.json')
+obj = bucket.object('data/DPR_PublicArt_001.json')
+obj.put({acl: "public-read", body: response.body})
